f913a19ce139bf66136a0defb2ecc027e8c4f0b4
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2022-08-25 13:08:19 -0400

    sidebar nav animations

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,9 @@
 import './index.scss';
 import LogoW from '../../assets/images/logo-w.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -9,6 +11,17 @@ const Sidebar = () => (
             <img src={LogoW} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="whitener" />
         </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
     </div>
 );
 
