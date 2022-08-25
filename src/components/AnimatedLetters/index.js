1c7ca8760431c056069af60dac7fde117bf02509
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2022-08-25 18:10:58 -0400

    animate home

---
+++ src/components/AnimatedLetters/index.js
@@ -0,0 +1,17 @@
+import './index.scss';
+
+const AnimatedLetters = ({ letterClass, strArray, delay }) => {
+    return (
+        <span>
+            {
+                strArray.map((char, i) =>(
+                    <span key={char + i} className={`${letterClass} _${i + delay}`}>
+                        {char}
+                    </span>
+                ))
+            }
+        </span>
+    )
+}
+
+export default AnimatedLetters;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,38 @@
 import './index.scss';
 import LogoTitle from '../../assets/images/logo-c.png'
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['a', 's', 'e', 'y', ' ', 'W', 'h', 'i', 't', 'e', 'n', 'e', 'r'];
+    const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
 
+    
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i,</span>
+                <br/>
+                <span className={`${letterClass} _13`}>I</span>
+                <span className={`${letterClass} _14`}>'m</span>
                 <img src={LogoTitle} alt="developer"/>
-                asey Whitener
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray}
+                delay={15}/>
                 <br />
-                Software Developer
+                <AnimatedLetters letterClass={letterClass}
+                strArray={jobArray}
+                delay={22}/>
                 </h1>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
         </div>
+        </>
     );
 }
 
