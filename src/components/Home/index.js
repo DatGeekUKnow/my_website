f899afeaea3cd7112e26baa3058222d1be8f3ac7
DatGeekUKnow <girjakeandallawesomepplz@gmail.com>
2022-08-26 13:32:34 -0400

    whitener homepage animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,6 +3,7 @@ import LogoTitle from '../../assets/images/logo-c.png'
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
+import Logo from './Logo';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -20,22 +21,23 @@ const Home = () => {
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span>
-                <br/>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span>
-                <img src={LogoTitle} alt="developer"/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                delay={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                delay={22}/>
+                  <span className={letterClass}>H</span>
+                  <span className={`${letterClass} _12`}>i,</span>
+                  <br/>
+                  <span className={`${letterClass} _13`}>I</span>
+                  <span className={`${letterClass} _14`}>'m</span>
+                  <img src={LogoTitle} alt="developer"/>
+                  <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    delay={15}/>
+                  <br />
+                  <AnimatedLetters letterClass={letterClass}
+                  strArray={jobArray}
+                  delay={22}/>
                 </h1>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
         </>
     );
