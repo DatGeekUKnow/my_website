1a1ccb47bfbe0599fe3b3e58baaa85e6b0bb78d4
Geek <whitenercasey@gmail.com>
2022-11-12 22:05:25 -0500

    About page add info

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,5 +1,26 @@
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
 const About = () => (
-    'Hello World!'
+    <div className='container about-page'>
+        <div className='text-zone'>
+            <h1>
+                <AnimatedLetters
+                    strArray={['A','b','o','u','t', ' ', 'M', 'e']}
+                    idx={15}
+                />
+            </h1>
+            <p>
+                I'm a very ambitious back-end developer looking for
+                a role at an established Tech company with the opportunity to work with the latest
+                technologies on challenging and diverse projects.
+            </p>
+            <p>
+                I'm confident, curious, and perpetually investigating emerging technologies.
+            </p>
+        </div>
+        
+    </div>
 );
 
 export default About;
